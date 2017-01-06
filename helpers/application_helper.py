3184470ed158210cefe5b6c2225909ae8ed553f0
cqcn1991 <dadazhu_ashi@163.com>
2017-01-06 11:12:49 +0800

    Add Rank Comparison

--- helpers/application_helper.py
+++ helpers/application_helper.py
@@ -2,20 +2,33 @@ from .shared_imports import *
 from .plot_print_helper import plt_configure
 
 
-def plot_movie_scatter_comparison(df, title=''):
+def plot_movie_scatter_comparison(df, title='', type='rank'):
     fig, ax = plt.subplots()
     # 1. plot scatter
-    scatter_plot = ax.scatter(df.db_rating, df.imdb_rating, alpha=0.3)
-    x = [0, 10]
+    if type == 'rating':
+        douban, imdb = df.db_rating, df.imdb_rating
+        scale = 10
+    else:
+        douban, imdb = df.db_rank, df.imdb_rank
+        scale = 100
+    x = [0, scale]
     plt.plot(x, x, '--', color='black', label='1:1')
 
     # 2. regression
-    fit = np.polyfit(df.db_rating, df.imdb_rating, 1)
+    fit = np.polyfit(douban, imdb, 1)
     fit_fn = np.poly1d(fit)
-    x = linspace(0, 10)
+    x = linspace(0, scale)
     plt.plot(x, fit_fn(x), '-', label='Regression')
+    if type != 'rating':
+        plt.fill_between(x, x - 20, x + 20, alpha=0.15)
 
-    plt.axis([0, 10, 0, 10])
-    plt_configure(xlabel='Douban Rating', ylabel='IMDB Rating', title=title, legend={'loc': 'best'})
+    # 3. Plot scatter
+    scatter_plot = ax.scatter(douban, imdb, alpha=0.3)
+
+    plt.axis([0, scale, 0, scale])
+    if type == 'rating':
+        plt_configure(xlabel='Douban Rating', ylabel='IMDB Rating', title=title, legend={'loc': 'best'})
+    else:
+        plt_configure(xlabel='Douban Rank', ylabel='IMDB Rank', title=title, legend={'loc': 'best'})
     plt.gca().set_aspect('equal')
     return fig, fit_fn, scatter_plot
