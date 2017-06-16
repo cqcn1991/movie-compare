4552bb71b3709e777becee0f7d2486ec651b6969
cqcn1991 <dadazhu_ashi@163.com>
2017-06-16 17:59:35 +0800

    fix cluster wrong file problem

--- views/view_loader.py
+++ views/view_loader.py
@@ -65,8 +65,24 @@ def turn_scatter_into_interactive(fig, scatter_plot, df, file_name, show_ratings
     return HTML(button)
 
 
-# def movie_index():
-#     movie_index_template = env.get_template('movie_index.jinjia')
-#     output_from_parsed_template = movie_index_template.render(movie_list_content=movie_list_content, css=css)
-#     with open("movie_index.html", "wb") as fh:
-#         fh.write(output_from_parsed_template)
+def scatter_interactive(fig, scatter_plot, df, file_name, figsize=False):
+    from jinja2 import Environment, FileSystemLoader
+    from mpld3 import plugins
+    from helpers.application_helper import PointClickableHTMLTooltip2
+    # file_path = './assets/interactive_plots/'+file_name
+    file_path = file_name
+    env = Environment(loader=FileSystemLoader('./views'))
+    restaurants = df.to_dict(orient='records')
+    restaurant_template = env.get_template('restaurant.jinja')
+    cards = [restaurant_template.render(restaurant=restaurant) for restaurant in restaurants]
+    if figsize:
+        fig.set_size_inches(figsize)
+    else:
+        fig.set_size_inches(8.5, 8.5)
+    plugins.connect(fig, PointClickableHTMLTooltip2(scatter_plot, labels=cards, targets=df['url'].tolist(), css=load_css(raw=True)))
+    mpld3.save_html(fig, file_path)
+    button = '''<a class='btn btn-default' style="text-decoration: none;;"
+    href="{0}" target='_blank'>
+    Interactive Scatter Plot</a>
+    '''.format(file_path)
+    return HTML(button)
